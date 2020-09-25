d8f5af1560424f5a2f5f8bbeeba08ec1f0db76a4
peakit <hmanak@gmail.com>
2020-09-25 08:06:35 +0530

    Adding examples for circuit breaker using netflix Hystrix with Spring boot

---
+++ circuit-breaker-reading/src/test/java/com/peakit/circuitbreakerreading/CircuitBreakerReadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peakit.circuitbreakerreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerReadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
